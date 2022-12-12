4d3cc8ff896cb1d4c3a26bd108292ce9dfc3de24
Steve-07 <55524682+Steve-07@users.noreply.github.com>
2022-12-12 15:33:26 -0800

    add PORT to env and add to repo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+const CONNECTION_URL = process.env.test.CONNECTION_URL;
 
 const PORT = process.env.PORT || 5000;
 
